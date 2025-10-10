ceceb56713734b1e45ce495a9cd9536272437fd8
Magloire Kouadio <magloire078@gmail.com>
2025-10-10 14:21:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2935714752-24641",
+  "appId": "1:996982313737:web:771dee1603e733470279d8",
+  "apiKey": "AIzaSyBxUyKltELPLjwxsFoVfSIHNEIZtYq0pS0",
+  "authDomain": "studio-2935714752-24641.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "996982313737"
+};
