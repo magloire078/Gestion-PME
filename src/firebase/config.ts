f275e361ada8f121fac747c1f2a7caad538c7ec7
Magloire Kouadio <magloire078@gmail.com>
2025-10-15 14:38:15 +0000

    ok

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2935714752-24641",
   "appId": "1:996982313737:web:771dee1603e733470279d8",
+  "storageBucket": "studio-2935714752-24641.appspot.com",
   "apiKey": "AIzaSyBxUyKltELPLjwxsFoVfSIHNEIZtYq0pS0",
   "authDomain": "studio-2935714752-24641.firebaseapp.com",
   "measurementId": "",
